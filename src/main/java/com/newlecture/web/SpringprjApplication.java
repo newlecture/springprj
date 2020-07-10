a6c366ff485a8473b255ef320837735e9c7d0fee
newlec <gituser@newlecture.com>
2020-07-10 21:41:58 +0900

    프로젝트 첫 커밋

---
+++ src/main/java/com/newlecture/web/SpringprjApplication.java
@@ -0,0 +1,13 @@
+package com.newlecture.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newlecture/web/SpringprjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newlecture.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
